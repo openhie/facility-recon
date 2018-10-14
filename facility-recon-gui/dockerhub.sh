fd591613de2d0c467a092984422a265aaf4fd65c
Richard Stanley <richardlstanley@gmail.com>
2018-10-13 20:43:59 -0400

    beginning dockerization

---
+++ facility-recon-gui/dockerhub.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ex
+
+# automate tagging with the short commit hash
+docker build --no-cache -t openhie/facility-recon-gui:$(git rev-parse --short HEAD) .
+docker tag openhie/facility-recon-gui:$(git rev-parse --short HEAD) openhie/facility-recon-gui
+docker push openhie/facility-recon-gui:$(git rev-parse --short HEAD)
+docker push openhie/facility-recon-gui:latest
